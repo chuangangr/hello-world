f628272b4d831d86d76d5adef950dc90895b6d4d
Chuangang Ren <chuangang@chuangang-macbookpro.roam.corp.google.com>
2016-10-13 22:43:13 -0700

    push

--- read.py
+++ read.py
@@ -5,7 +5,6 @@ with open('./test.txt', 'r') as f:
 t = [e.strip() for e in lines]
 print t
 
-
 tt = [l.strip() for l in open('./test.txt', 'r')]
 print tt
 
@@ -16,15 +15,29 @@ def input_arg1(a, *args):
 	print("Print out args: %s"% a)
 	for i in args: 
 		print i
-
 input_arg1('input1', 'input2', 'input3', 'input4')
-
 
 
 def input_arg2(a, **args):
 	print("Print out args: %s"% a)
 	for i in args: 
-		print i
+		print "key: ", i, ", Value: ", args[i]
+
+input_arg2('first arg', gang='charles', nan='cindy')
+
+def test_var_args_call(arg1, arg2, arg3):
+    print "arg1:", arg1
+    print "arg2:", arg2
+    print "arg3:", arg3
+
+args = ("two", 3)
+test_var_args_call(1, *args)
 
 
-input_arg1('input1', {'A':'input2', 'B':'input3', 'C':'input4'})
+def test_var_args_call(arg1, arg2, arg3):
+    print "arg1:", arg1
+    print "arg2:", arg2
+    print "arg3:", arg3
+
+kwargs = {"arg3": 3, "arg2": "two"}
+test_var_args_call(1, **kwargs)
